76a1a7ac15c2c0298d1445fde80b3512852b4100
enchart <77974051+enchart@users.noreply.github.com>
2024-05-14 01:32:40 +0500

    fix default store values

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,4 +2,4 @@ import { persisted } from 'svelte-persisted-store';
 
 export const coins = persisted('coins', 0);
 export const button = persisted('button', 1);
-export const background = persisted('background', 2);
+export const background = persisted('background', 1);
